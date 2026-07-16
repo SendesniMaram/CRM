342833c9677c18d308e52c1072f1d4265155e249
SendesniMaram <maryam25082018@gmail.com>
2026-07-16 19:01:29 +0200

    Infrastructure : ajout du service Discovery (Eureka Server)

---
+++ microservices/discovery-service/src/test/java/com/crm/discovery/DiscoveryServiceSmokeTest.java
@@ -0,0 +1,14 @@
+package com.crm.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceSmokeTest {
+
+    @Test
+    void contextLoads() {
+        // If the Spring context starts successfully, Eureka Server wiring is valid.
+    }
+}
+
